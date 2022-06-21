e2f4192a677494e487d4a8a52233c986caa37017
Victor Pogor <victor.pogor@outlook.com>
2022-06-21 21:41:09 +0300

    feat(primitives): boolean object adjustments

--- src/Off.Net.Pdf.Core/Primitives/PdfBoolean.cs
+++ src/Off.Net.Pdf.Core/Primitives/PdfBoolean.cs
@@ -7,12 +7,13 @@ using Off.Net.Pdf.Core.Interfaces;
 /// Represents a PDF Boolean object.
 /// </summary>
 /// <seealso cref="IPdfObject{bool}" />
-public struct PdfBoolean : IPdfObject<bool>, IEquatable<PdfBoolean>
+public struct PdfBoolean : IPdfObject<bool>, IEquatable<PdfBoolean>, IComparable, IComparable<PdfBoolean>
 {
     private const string FalseLiteral = "false";
     private const string TrueLiteral = "true";
     private static readonly byte[] FalseBytes = Encoding.ASCII.GetBytes(FalseLiteral);
     private static readonly byte[] TrueBytes = Encoding.ASCII.GetBytes(TrueLiteral);
+    private static readonly int primitiveHashCodePrefix = nameof(PdfBoolean).GetHashCode();
 
     /// <summary>
     /// Initializes a new instance of the <see cref="PdfBoolean"/> struct.
@@ -40,31 +41,59 @@ public struct PdfBoolean : IPdfObject<bool>, IEquatable<PdfBoolean>
     public byte[] Bytes => Value ? TrueBytes : FalseBytes;
 
     /// <inheritdoc/>
-    public override int GetHashCode() => Value.GetHashCode();
+    public override int GetHashCode() => HashCode.Combine(primitiveHashCodePrefix, Value.GetHashCode());
 
     /// <inheritdoc/>
     public override string ToString() => Value ? TrueLiteral : FalseLiteral;
 
     #region Operators
     /// <summary>
+    /// Implements the operator ==.
+    /// </summary>
+    /// <param name="leftOperator">The left operator.</param>
+    /// <param name="rightOperator">The right operator.</param>
+    /// <returns>The result of the operator.</returns>
+    public static bool operator ==(PdfBoolean leftOperator, PdfBoolean rightOperator) => leftOperator.CompareTo(rightOperator) == 0;
+
+    /// <summary>
     /// Implements the operator !=.
     /// </summary>
     /// <param name="leftOperator">The left operator.</param>
     /// <param name="rightOperator">The right operator.</param>
-    /// <returns>
-    /// The result of the operator.
-    /// </returns>
-    public static bool operator !=(PdfBoolean leftOperator, PdfBoolean rightOperator) => !leftOperator.Equals(rightOperator);
+    /// <returns>The result of the operator.</returns>
+    public static bool operator !=(PdfBoolean leftOperator, PdfBoolean rightOperator) => leftOperator.CompareTo(rightOperator) != 0;
 
     /// <summary>
     /// Implements the operator ==.
     /// </summary>
     /// <param name="leftOperator">The left operator.</param>
     /// <param name="rightOperator">The right operator.</param>
-    /// <returns>
-    /// The result of the operator.
-    /// </returns>
-    public static bool operator ==(PdfBoolean leftOperator, PdfBoolean rightOperator) => leftOperator.Equals(rightOperator);
+    /// <returns>The result of the operator.</returns>
+    public static bool operator <(PdfBoolean leftOperator, PdfBoolean rightOperator) => leftOperator.CompareTo(rightOperator) < 0;
+
+    /// <summary>
+    /// Implements the operator ==.
+    /// </summary>
+    /// <param name="leftOperator">The left operator.</param>
+    /// <param name="rightOperator">The right operator.</param>
+    /// <returns>The result of the operator.</returns>
+    public static bool operator <=(PdfBoolean leftOperator, PdfBoolean rightOperator) => leftOperator.CompareTo(rightOperator) <= 0;
+
+    /// <summary>
+    /// Implements the operator ==.
+    /// </summary>
+    /// <param name="leftOperator">The left operator.</param>
+    /// <param name="rightOperator">The right operator.</param>
+    /// <returns>The result of the operator.</returns>
+    public static bool operator >(PdfBoolean leftOperator, PdfBoolean rightOperator) => leftOperator.CompareTo(rightOperator) > 0;
+
+    /// <summary>
+    /// Implements the operator ==.
+    /// </summary>
+    /// <param name="leftOperator">The left operator.</param>
+    /// <param name="rightOperator">The right operator.</param>
+    /// <returns>The result of the operator.</returns>
+    public static bool operator >=(PdfBoolean leftOperator, PdfBoolean rightOperator) => leftOperator.CompareTo(rightOperator) >= 0;
 
     /// <summary>
     /// Performs an implicit conversion from <see cref="PdfBoolean"/> to <see cref="bool"/>.
@@ -81,11 +110,47 @@ public struct PdfBoolean : IPdfObject<bool>, IEquatable<PdfBoolean>
     public static implicit operator PdfBoolean(bool value) => new(value);
     #endregion
 
-    #region Equality checking methods
+    #region IEquatable implementations
     /// <inheritdoc/>
     public bool Equals(PdfBoolean other) => Value == other.Value;
 
     /// <inheritdoc/>
     public override bool Equals(object? obj) => (obj is PdfBoolean booleanObject) && Equals(booleanObject);
+    #endregion
+
+    #region IComparable implementations
+    /// <summary>
+    /// Compares the current instance with another object of the same type and returns an integer that
+    /// indicates whether the current instance precedes, follows, or occurs in the same position in the sort
+    /// order as the other object.
+    /// </summary>
+    /// <param name="other">An object to compare with this instance.</param>
+    /// <returns>A value that indicates the relative order of the objects being compared.</returns>
+    public int CompareTo(PdfBoolean other)
+    {
+        if (Value == other.Value)
+        {
+            return 0;
+        }
+
+        return Value ? 1 : -1;
+    }
+
+    /// <summary>
+    /// Compares the current instance with another object of the same type and returns an integer that indicates whether
+    /// the current instance precedes, follows, or occurs in the same position in the sort order as the other object.
+    /// </summary>
+    /// <param name="obj">An object to compare with this instance.</param>
+    /// <returns>A value that indicates the relative order of the objects being compared.</returns>
+    /// <exception cref="ArgumentException"><paramref name="obj" /> is not the same type as this instance.</exception>
+    public int CompareTo(object? obj)
+    {
+        if (obj is not PdfBoolean pdfBoolean)
+        {
+            throw new ArgumentException("Object must be of PDF Boolean type.");
+        }
+
+        return CompareTo(pdfBoolean);
+    }
     #endregion
 }
--- tests/Off.Net.Pdf.Core.Tests/Primitives/PdfBooleanTests.cs
+++ tests/Off.Net.Pdf.Core.Tests/Primitives/PdfBooleanTests.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json.Linq;
+using System;
 using Off.Net.Pdf.Core.Primitives;
 using Xunit;
 
@@ -130,17 +132,113 @@ public class PdfBooleanTests
     }
 
     [Theory(DisplayName = "Check if GetHashCode method returns valid value")]
-    [InlineData(false, 0)]
-    [InlineData(true, 1)]
-    public void PdfBoolean_GetHashCode_CheckValidity(bool value1, int expectedHashCode)
+    [InlineData(false)]
+    [InlineData(true)]
+    public void PdfBoolean_GetHashCode_CheckValidity(bool value1)
     {
         // Arrange
         PdfBoolean pdfBoolean1 = value1; // Use an implicit conversion from bool to PdfBoolean
+        int expectedHashCode = HashCode.Combine(nameof(PdfBoolean).GetHashCode(), value1.GetHashCode());
 
         // Act
         int actualHashCode = pdfBoolean1.GetHashCode();
 
         // Assert
         Assert.Equal(expectedHashCode, actualHashCode);
+    }
+
+    [Theory(DisplayName = "Check if comparison operators works properly")]
+    [InlineData(false, false, "==", true)]
+    [InlineData(false, false, "!=", false)]
+    [InlineData(false, false, "<", false)]
+    [InlineData(false, false, "<=", true)]
+    [InlineData(false, false, ">", false)]
+    [InlineData(false, false, ">=", true)]
+    [InlineData(false, true, "==", false)]
+    [InlineData(false, true, "!=", true)]
+    [InlineData(false, true, "<", true)]
+    [InlineData(false, true, "<=", true)]
+    [InlineData(false, true, ">", false)]
+    [InlineData(false, true, ">=", false)]
+    [InlineData(true, false, "==", false)]
+    [InlineData(true, false, "!=", true)]
+    [InlineData(true, false, "<", false)]
+    [InlineData(true, false, "<=", false)]
+    [InlineData(true, false, ">", true)]
+    [InlineData(true, false, ">=", true)]
+    [InlineData(true, true, "==", true)]
+    [InlineData(true, true, "!=", false)]
+    [InlineData(true, true, "<", false)]
+    [InlineData(true, true, "<=", true)]
+    [InlineData(true, true, ">", false)]
+    [InlineData(true, true, ">=", true)]
+    public void PdfBoolean_Operators_CheckValidity(bool value1, bool value2, string op, bool expectedResult)
+    {
+        // Arrange
+        PdfBoolean pdfBoolean1 = value1; // Use an implicit conversion from bool to PdfBoolean
+        PdfBoolean pdfBoolean2 = value2; // Use an implicit conversion from bool to PdfBoolean
+
+        // Act
+        bool actualResult = op switch
+        {
+            "==" => pdfBoolean1 == pdfBoolean2,
+            "!=" => pdfBoolean1 != pdfBoolean2,
+            "<" => pdfBoolean1 < pdfBoolean2,
+            "<=" => pdfBoolean1 <= pdfBoolean2,
+            ">" => pdfBoolean1 > pdfBoolean2,
+            ">=" => pdfBoolean1 >= pdfBoolean2,
+            _ => throw new ArgumentException("Invalid operation", nameof(op)),
+        };
+
+        // Assert
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [Theory(DisplayName = "Check if implicit operator works")]
+    [InlineData(false)]
+    [InlineData(true)]
+    public void PdfBoolean_CheckImplicitOperator(bool value1)
+    {
+        // Arrange
+        var pdfBoolean1 = new PdfBoolean(value1);
+
+        // Act
+        bool actualValue = pdfBoolean1; // Use an implicit conversion from PdfBoolean to bool
+        bool expectedValue = pdfBoolean1.Value;
+
+        // Assert
+        Assert.Equal(expectedValue, actualValue);
+    }
+
+    [Theory(DisplayName = "Check CompareTo method")]
+    [InlineData(false, false, 0)]
+    [InlineData(false, true, -1)]
+    [InlineData(true, false, 1)]
+    [InlineData(true, true, 0)]
+    public void PdfBoolean_CompareTo_CheckValidity(bool value1, bool value2, int expectedValue)
+    {
+        // Arrange
+        var pdfBoolean1 = new PdfBoolean(value1);
+        object pdfBoolean2 = new PdfBoolean(value2);
+
+        // Act
+        int actualValue = pdfBoolean1.CompareTo(pdfBoolean2);
+
+        // Assert
+        Assert.Equal(expectedValue, actualValue);
+    }
+
+    [Fact(DisplayName = "Check CompareTo method should throw an exception")]
+    public void PdfBoolean_CompareTo_ThrowsException()
+    {
+        // Arrange
+        var pdfBoolean1 = new PdfBoolean();
+        object pdfBoolean2 = 5;
+
+        // Act
+        Action actualValueDelegate = () => pdfBoolean1.CompareTo(pdfBoolean2);
+
+        // Assert
+        Assert.Throws<ArgumentException>(actualValueDelegate);
     }
 }
